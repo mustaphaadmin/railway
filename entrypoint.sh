b542110078ac3b5cbc2a9f01fca469210a37c3b3
mustaphaadmin <50841167+mustaphaadmin@users.noreply.github.com>
2025-06-19 22:33:41 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /app/output
 
-ffmpeg -re -i "http://iboeag.cdn-dtv.com/live/u5965466rtdy/byv3ayhv9q/1388298.m3u8" \
+ffmpeg -re -i "http://iboeag.cdn-dtv.com/live/u5965466rtdy/byv3ayhv9q/1390827.m3u8" \
 -c copy -f hls -hls_time 4 -hls_list_size 5 -hls_flags delete_segments \
 /app/output/stream.m3u8 &
 
